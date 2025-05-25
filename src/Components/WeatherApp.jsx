1e2de823adc47b75ec4834354a7c6785d2218513
Shivansh <shivansh2162001@gmail.com>
2025-05-25 17:14:48 -0600

    Structured the app properly and worked on some data fetching of the application

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -3,8 +3,32 @@ import cloudy from "../assets/images/cloudy.png"
 import rainy from "../assets/images/rainy.png"
 import snowy from "../assets/images/snowy.png"
 import loading from "../assets/images/loading.gif"
+import { useState } from "react"
 
 const WeatherApp = () => {
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState('')
+    const api_key = 'c4af425271c7865194f73b48922ab80a'
+
+    const handleInputChange = (e) =>{
+        setLocation(e.target.value)
+    }
+
+    const search = async () => {
+        // https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid=c4af425271c7865194f73b48922ab80a
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${api_key}`
+        const response = await fetch(url)
+        const searchData = await response.json()
+        setData(searchData)
+        setLocation('')
+        console.log(searchData)
+    }
+
+    const handleKeyDown = (e) => {
+        if(e.key === "Enter")
+            search()
+    }
+
     return(
         //  The main app container where all our weather UI will live.
         <div className="container"> 
@@ -12,18 +36,21 @@ const WeatherApp = () => {
                 <div className="search">
                     <div className="search-top">
                         <i className="fa-solid fa-location-dot"></i>
-                        <div className="location">Canada</div>
+                        <div className="location">{data.name}</div>
                     </div>
                     <div className="search-bar">
-                        <input type="text" placeholder="Enter Location"/>
-                        <i className="fa-solid fa-magnifying-glass"></i>
+                        <input type="text" placeholder="Enter Location"
+                         value={location}
+                         onChange={handleInputChange}
+                         onKeyDown={handleKeyDown}/>
+                        <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
                     </div>
                 </div>
 
                 <div className="weather">
                     <img src={sunny} alt="sunny"/>
                     <div className="weather-type">Clear</div>
-                    <div className="temp">28°C</div>
+                    <div className="temp">{data.main? `${Math.floor(data.main.temp)}°C`: null}</div>
                 </div>
 
                 <div className="weather-date">
