1acba92fd6723882f50a68f739dc1e23571e1659
Shivansh <shivansh2162001@gmail.com>
2025-05-26 16:32:23 -0600

    Retrieving the data from the api and handling errors

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -3,25 +3,40 @@ import cloudy from "../assets/images/cloudy.png"
 import rainy from "../assets/images/rainy.png"
 import snowy from "../assets/images/snowy.png"
 import loading from "../assets/images/loading.gif"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
 
 const WeatherApp = () => {
     const [data, setData] = useState({})
     const [location, setLocation] = useState('')
     const api_key = 'c4af425271c7865194f73b48922ab80a'
+    
+    useEffect(()=>{
+    const fetchDefaultWeather = async () => {
+        const defaultLocation = "Edmonton"
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&units=Metric&appid=c4af425271c7865194f73b48922ab80a`
+        const res = await fetch(url)
+        const defaultData = await res.json()
+        setData(defaultData)
+    }   
+    fetchDefaultWeather()
+    }, [])
 
     const handleInputChange = (e) =>{
         setLocation(e.target.value)
     }
 
     const search = async () => {
-        // https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid=c4af425271c7865194f73b48922ab80a
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${api_key}`
-        const response = await fetch(url)
-        const searchData = await response.json()
-        setData(searchData)
-        setLocation('')
-        console.log(searchData)
+        if(location.trim() !== "")
+        {
+            // https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid=c4af425271c7865194f73b48922ab80a
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${api_key}`
+            const response = await fetch(url)
+            const searchData = await response.json()
+            setData(searchData)
+            setLocation('')
+            console.log(searchData)
+        }
     }
 
     const handleKeyDown = (e) => {
@@ -49,8 +64,8 @@ const WeatherApp = () => {
 
                 <div className="weather">
                     <img src={sunny} alt="sunny"/>
-                    <div className="weather-type">Clear</div>
-                    <div className="temp">{data.main? `${Math.floor(data.main.temp)}°C`: null}</div>
+                    <div className="weather-type">{data.weather ? data.weather[0].main: null}</div>
+                    <div className="temp">{data.main ? `${Math.floor(data.main.temp)}°C`: null}</div>
                 </div>
 
                 <div className="weather-date">
@@ -60,13 +75,13 @@ const WeatherApp = () => {
                     <div className="humidity">
                         <div className="data-name">Humidity</div>
                         <i className="fa-solid fa-droplet"></i>
-                        <div className="data">35%</div>
+                        <div className="data">{data.main ? `${(data.main.humidity)}%`:null}</div>
                     </div>
                     
                     <div className="wind">
                         <div className="data-name">Wind</div>
                         <i className="fa-solid fa-wind"></i>
-                        <div className="data">3 km/h</div>
+                        <div className="data"> {data.wind ? `${(data.wind.speed)} km/h`: null}</div>
                     </div>
                 </div>
             </div>
